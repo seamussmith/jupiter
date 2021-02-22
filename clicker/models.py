eedc895d8d4e3cb99fb1eacf1ee8f632d35c7d6e
seamussmith <student.141504@worcesterschools.net>
2021-02-22 16:51:51 -0500

    Added prototype for button model

--- clicker/models.py
+++ clicker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Button(models.Models):
+    count = models.IntegerField()
+    text = models.TextField()
+    
+    @staticmethod
+    def new(text):
+        return Button(
+            count = 0,
+            text = text
+        )
+    
+    def like(self):
+        self.count += 1
+    def dislike(self):
+        self.count -= 1
+    def reset(self):
+        self.count = 0
