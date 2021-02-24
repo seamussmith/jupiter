14c207e4884c469b67dcd6fe8baf7c7d6ef6ce45
seamussmith <student.141504@worcesterschools.net>
2021-02-24 11:44:13 -0500

    Add handler for button clicks

--- clicker/urls.py
+++ clicker/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "clicker"
 urlpatterns = [
-   path("", views.index, name="index")
+   path("", views.index, name="index"),
+   path("handle-click/", views.handle_click, name="handle-click")
 ]
--- clicker/views.py
+++ clicker/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
+from django.views.decorators.http import require_http_methods
 # Create your views here.
 
 def index(request):
     return render(request, "pages/index.html", context={
         "buttons": list(Button.objects.all())
     })
+
+@require_http_methods(["POST"])
+def handle_click(request):
+    body = request.POST
+    btn = Button.objects.get(id=body["uuid"])
+    if body["val"] == "like":
+        btn.like()
+    elif body["val"] == "dislike" and btn.count != 0:
+        btn.dislike()
+    btn.save()
+    return redirect("clicker:index")
