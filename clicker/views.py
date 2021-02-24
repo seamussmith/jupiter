dc9a006c2700db4009eedc839bc32eef29992582
seamussmith <student.141504@worcesterschools.net>
2021-02-24 13:24:07 -0500

    Anti-spam

--- clicker/views.py
+++ clicker/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from .models import *
 from django.views.decorators.http import require_http_methods
+import time
 # Create your views here.
 
 def index(request):
@@ -19,9 +21,17 @@ def handle_click(request):
     btn.save()
     return redirect("clicker:index")
 
+SPAM_DELAY = 20
 @require_http_methods(["POST"])
 def new_button(request):
     body = request.POST
+    curr_time = time.time()
+    if request.session.get("last_submit") is not None:
+        last_time = request.session["last_submit"]
+        delta = curr_time - last_time
+        if delta < SPAM_DELAY:
+            return HttpResponse(f"Dont spam! Please wait {round(20 - delta)} more seconds!")
+    request.session["last_submit"] = time.time()
     btn = Button.new(body["text"])
     btn.save()
     return redirect("clicker:index")
