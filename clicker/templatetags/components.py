f7d505c6d7b30fcc24b902935fd996dd0c3fe654
seamussmith <student.141504@worcesterschools.net>
2021-02-24 11:44:00 -0500

    refactor button component

--- clicker/templatetags/components.py
+++ clicker/templatetags/components.py
@@ -1,8 +1,7 @@
 from django.template.defaulttags import register
 
 @register.inclusion_tag("components/button.html")
-def Button(button_uuid, button_text):
+def Button(button):
     return {
-        "button_uuid": button_uuid,
-        "button_text": button_text
+        "button": button
     }
