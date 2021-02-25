9f2941ed72aecbea388dbaec3028aed547f2a558
seamussmith <student.141504@worcesterschools.net>
2021-02-25 12:28:22 -0500

    Implment AJAX submition for REST api

---
+++ static/ts/like-script.ts
@@ -0,0 +1,21 @@
+
+interface Button
+{
+    id: number
+    text: string
+    count: number
+}
+
+$(".vote-button").on("click", function(e) {
+    let $parent = $(this).parent()
+    $.ajax({
+        method: $parent.attr("method"),
+        url: $parent.attr("action"),
+        data: `${$parent.serialize()}&val=${$(this).attr("value")}`
+    })
+    .done((data: Button) => {
+        $parent.find(".likes").text(data.count)
+        $parent.find("button").prop("disabled", false)
+    })
+    $parent.find("button").prop("disabled", true)
+})
