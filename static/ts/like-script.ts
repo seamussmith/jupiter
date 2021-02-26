5da0f9f67a9301fd693e79495571720df1c5b3f5
seamussmith <student.141504@worcesterschools.net>
2021-02-26 09:25:19 -0500

    Started classing the button components

--- static/ts/like-script.ts
+++ static/ts/like-script.ts
@@ -6,7 +6,7 @@ interface Button
     count: number
 }
 
-$(".vote-button").on("click", function(e) {
+$(".liker__vote-button").on("click", function(e) {
     let $parent = $(this).parent()
     $.ajax({
         method: $parent.attr("method"),
@@ -14,7 +14,7 @@ $(".vote-button").on("click", function(e) {
         data: `${$parent.serialize()}&val=${$(this).attr("value")}`
     })
     .done((data: Button) => {                           // POST returns a JSONResponse, and JQuery automatically parses JSON
-        $parent.find(".likes").text(data.count)         // Probs something about MIME types
+        $parent.find(".liker__likes").text(data.count)         // Probs something about MIME types
         $parent.find("button").prop("disabled", false)
     })
     $parent.find("button").prop("disabled", true)
